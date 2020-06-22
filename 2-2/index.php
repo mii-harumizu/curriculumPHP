1998beb4f3148a6aa7f772abc0c75021d5dbf1d3
mii-harumizu <66872436+mii-harumizu@users.noreply.github.com>
2020-06-22 19:53:54 +0900

    課題提出

---
+++ 2-2/index.php
@@ -0,0 +1,45 @@
+<?php
+$message = "Hello World!";
+echo $message;
+
+echo "<br>";
+
+$x = 8;
+$y = 5.55;
+echo $x;
+echo "<br>";
+echo $y;
+echo "<br>";
+var_dump($x);
+echo "<br>";
+var_dump($y)
+?>
+
+<?php
+echo "<br>";
+// 理論値
+$flag = true;
+var_dump($flag);
+echo "<br>";
+// NULL
+$emp = null;
+var_dump($emp);
+?>
+
+<?php
+// 日本語
+$en_words = "Hello World";
+var_dump($en_words);
+echo "<br>";
+$ja_words = "こんにちは";
+var_dump($ja_words);
+echo "<br>"
+?>
+
+<?php
+echo "<h1>用語課題</h1>";
+echo "<br>";
+echo "<p><strong>API</strong>はアプリケーション、ソフトウェアとプログラムをつなぐもの。外部アプリとの連携を可能にする</p>";
+echo "<br>";
+echo "<p><strong>SVN</strong>はソースコードや様々なデータを管理するために用いられるシステム。バージョン管理システム。</p>";
+?>
