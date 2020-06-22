d99e91037d24e39f11a66bbcc6554758886e6835
mii-harumizu <66872436+mii-harumizu@users.noreply.github.com>
2020-06-23 00:41:08 +0900

    課題提出

---
+++ 2-4/index.php
@@ -0,0 +1,33 @@
+<?php
+$hello = "Hello";
+$world = "world";
+echo $hello . $world;
+echo "<br>";
+echo 10 - 9;
+echo "<br>";
+echo 10 * 9;
+echo "<br>";
+echo 10 / 2;
+echo "<br>";
+echo 10 % 3;
+echo "<br>";
+echo 10 * 4;
+echo "<br>";
+
+$x =5; 
+$x +=5;
+echo $x;
+echo "<br>";
+// 上から順番に計算
+$x *=6;
+echo $x;
+
+echo "<br>";
+echo "<h1>用語集</h1>";
+echo "<br>";
+echo "<p><strong>エビデンス</strong>:最終検証段階においてシステムが発注通りに動作していることを示す証拠となる書類やデータなどのこと</p>";
+echo "<br>";
+echo "<p><strong>バッチ</strong>:あらかじめ登録した一連の処理を自動的に実行する処理方式を示す。複数のプログラムファイルやファイル転送コマンドなどの実行順序を定義して大量データを一括処理する</p>";
+echo "<br>";
+echo "<p><strong>MVCモデル</strong>:ソフトウェアの設計モデルの一つで機能を「モデル」「ビュー」「コントローラー」の三つの役割に分離して実装しそれらが連携して処理を進める方式</p>";
+?>
