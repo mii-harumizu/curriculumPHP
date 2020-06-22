e218ddae035e02fb66c0d4973121798278342f78
mii-harumizu <66872436+mii-harumizu@users.noreply.github.com>
2020-06-22 22:29:01 +0900

    課題提出

---
+++ 2-3/index.php
@@ -0,0 +1,18 @@
+<?php
+define("ADMIN_EMAIL", "y-i-group@gmail.com");
+define("LIST_COUNT", 15);
+
+echo ADMIN_EMAIL;
+echo "<br>";
+echo LIST_COUNT;
+
+echo "<br>";
+echo "<h1>用語集</h1>";
+echo "<br>";
+echo "<p><strong>デプロイ</strong>:開発したアプリケーションをサーバ上に展開、配置して利用できるようにすること</p>";
+echo "<br>";
+echo "<p><strong>バリデーション</strong>:対象がその仕様や文法などに適切に記述、構築されているかどうかを検証する</p>";
+echo "<br>";
+echo "<p><strong>アジャイル・ウォーターフォール</strong>:アジャイル開発はスピーディな開発手法で短期の反復期間内でどんどん機能を追加していく手法。ウォーターフォール開発は開発作業を複数の工程に分けて、上流から下流へ水が流れるかのようにソフトウェア開発を行う手法</p>";
+
+?>
