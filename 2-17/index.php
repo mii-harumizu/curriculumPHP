654998b1b34b94397ec8f86797a4b069ec388acd
mii-harumizu <66872436+mii-harumizu@users.noreply.github.com>
2020-07-04 18:41:52 +0900

    課題提出

---
+++ 2-17/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<p>-サイコロ-</p>
+<?php
+$measure = 40;
+$count = 0;
+while($measure >= 0){
+  $dice = mt_rand(1, 6);
+  $count++;
+  $measure -= $dice;
+    echo $count. "回目＝". $dice. "<br>";
+  }
+  echo $count . "回目でゴール" . "<br>";
+  ?>
+
+<p>-時間別挨拶-</p>
+
+<?php
+
+$timestanp = strtotime("2010/1/1 10:00:00");
+// echo $timestanp;
+
+// $time = date("H", time());//現在の時間で
+$time = date("H", $timestanp);//指定の時間で
+
+
+if($time <= 11){
+  echo "今は" . $time . "時台です" . "<br>";
+  echo "おはようございます";
+}elseif($time === 12){
+  echo "今は" . $time . "時台です" . "<br>";
+  echo "こんにちは";  
+}elseif($time >= 13){
+  echo "今は" . $time . "時台です" . "<br>";
+  echo "こんばんは";  
+}
+
+?>
+
+</body>
+</html>
